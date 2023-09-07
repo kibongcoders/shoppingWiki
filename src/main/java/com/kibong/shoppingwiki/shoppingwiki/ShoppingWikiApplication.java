15ce4a2e272315f4f2effbe11bd56031b3e75cf1
kibongcoders <kkp02052@gmail.com>
2023-09-07 22:11:01 +0900

    Initial commit

---
+++ src/main/java/com/kibong/shoppingwiki/shoppingwiki/ShoppingWikiApplication.java
@@ -0,0 +1,13 @@
+package com.kibong.shoppingwiki.shoppingwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingWikiApplication.class, args);
+    }
+
+}
